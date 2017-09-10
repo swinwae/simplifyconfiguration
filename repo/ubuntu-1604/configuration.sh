2c768614dfd08a204ddd732fc6a1cd0a712bf49d
Trist <1223562701@qq.com>
2017-09-10 11:26:45 +0800

    refine conf file

--- repo/ubuntu-1604/configuration.sh
+++ repo/ubuntu-1604/configuration.sh
@@ -2,7 +2,7 @@
 # save your source list
 cp /etc/apt/sources.list /etc/apt/sources.list.backup
 # load content to list
-curl https://raw.githubusercontent.com/swinwae/simplifyconfiguration/wanlizhu/repo/ubuntu-1604/sources.list -O /etc/apt/sources.list
+curl -g 'https://raw.githubusercontent.com/swinwae/simplifyconfiguration/wanlizhu/repo/ubuntu-1604/sources.list' -O /etc/apt/sources.list
 # update repo
-apt-get update
-apt-get upgrade
+#apt-get update
+#apt-get upgrade
