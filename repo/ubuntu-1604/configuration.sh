ccfeedfb22d97810dbff6b628c946f038429bae9
Trist <1223562701@qq.com>
2017-09-10 10:21:43 +0800

    add ubuntu 16.04 shell command to update aliyun repo

---
+++ repo/ubuntu-1604/configuration.sh
@@ -0,0 +1,7 @@
+# save your source list
+cp /etc/apt/sources.list /etc/apt/sources.list.backup
+# load content to list
+wget -O /etc/apt/sources.list https://raw.githubusercontent.com/swinwae/simplifyconfiguration/wanlizhu/repo/ubuntu-1604/sources.list
+# update repo
+apt-get update
+apt-get upgrade
