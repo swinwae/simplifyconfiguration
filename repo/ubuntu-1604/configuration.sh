c4f40651022c254d730c44dad6016d861a2888d4
Trist <1223562701@qq.com>
2017-09-10 11:15:28 +0800

    use curl to download file content and load it to sources.list

--- repo/ubuntu-1604/configuration.sh
+++ repo/ubuntu-1604/configuration.sh
@@ -1,7 +1,7 @@
 # save your source list
 cp /etc/apt/sources.list /etc/apt/sources.list.backup
 # load content to list
-wget -O /etc/apt/sources.list https://raw.githubusercontent.com/swinwae/simplifyconfiguration/wanlizhu/repo/ubuntu-1604/sources.list
+curl https://raw.githubusercontent.com/swinwae/simplifyconfiguration/wanlizhu/repo/ubuntu-1604/sources.list -O /etc/apt/sources.list
 # update repo
 apt-get update
 apt-get upgrade
